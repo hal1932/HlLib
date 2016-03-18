fb6da644b0dd0e69dfa1ea1faf6865a752da9a00
hal1932 <yu.arai.19@gmail.com>
2016-03-18 22:10:33 +0900

    配列のスライス関数を追加

---
+++ HlLib/extensions/ArrayExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HlLib
+{
+    public static class ArrayExtensions
+    {
+        /// <summary>
+        /// Python風の配列スライス、source[startIndex]からsource[endIndex-1]までを返す
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="source"></param>
+        /// <param name="startIndex"></param>
+        /// <param name="endIndex">-1の場合はsource.Lengthが指定されたとみなす</param>
+        /// <returns></returns>
+        public static T[] Slice<T>(this T[] source, int startIndex, int endIndex = -1)
+            where T : new()
+        {
+            var count = (endIndex != -1) ?
+                endIndex - startIndex : source.Length - startIndex;
+            var destination = new T[count];
+            Array.Copy(source, startIndex, destination, 0, count);
+            return destination;
+        }
+    }
+}
