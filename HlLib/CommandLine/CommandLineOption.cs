db62f4ca2c4fd1b3d53b1bb1f62ffae0f2d513ab
hal1932 <yu.arai.19@gmail.com>
2016-03-18 20:46:39 +0900

    名前空間を追加

--- HlLib/CommandLine/CommandLineOption.cs
+++ HlLib/CommandLine/CommandLineOption.cs
@@ -1,11 +1,7 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace HlLib
+namespace HlLib.CommandLine
 {
     [AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
     public class CommandLineArgAttribute : Attribute
