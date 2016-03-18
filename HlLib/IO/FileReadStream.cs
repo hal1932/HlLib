2e5a2872f405cbdb37d58b2cb32a27c536cdf91a
hal1932 <yu.arai.19@gmail.com>
2016-03-18 21:12:20 +0900

    ストリーム読み書きの基本クラスを追加

---
+++ HlLib/IO/FileReadStream.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HlLib.IO
+{
+    public class FileReadStream : FileStream
+    {
+        public bool EndOfStream
+        {
+            get { return (Position < Length); }
+        }
+
+        public FileReadStream(string path, FileShare share = FileShare.Read)
+            : base(path, FileMode.Open, FileAccess.Read, share)
+        { }
+
+        public StreamReader CreateStringReader()
+        {
+            return new StreamReader(this);
+        }
+
+        public BinaryReader CreateBinaryReader()
+        {
+            return new BinaryReader(this);
+        }
+     }
+}
---
+++ HlLib/IO/FileWriteStream.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HlLib.IO
+{
+    public class FileWriteStream : FileStream
+    {
+        public FileWriteStream(string path, FileShare share = FileShare.None)
+            : base(path, FileMode.OpenOrCreate, FileAccess.Write, share)
+        { }
+
+        public StreamWriter CreateStringWriter()
+        {
+            return new StreamWriter(this);
+        }
+
+        public BinaryWriter CreateBinaryWriter()
+        {
+            return new BinaryWriter(this);
+        }
+    }
+}
