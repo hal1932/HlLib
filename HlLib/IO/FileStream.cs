803f2c35da260a2339acc0122a79c77d4a34cda4
hal1932 <yu.arai.19@gmail.com>
2016-03-22 21:01:19 +0900

    ファイルを整理

---
+++ HlLib/IO/FileStream.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+
+namespace HlLib.IO
+{
+    public class FileReadStream : FileStream
+    {
+        public FileReadStream(string path, FileShare share = FileShare.ReadWrite)
+            : base(path, FileMode.Open, FileAccess.Read, share)
+        { }
+    }
+
+    public class FileWriteStream : FileStream
+    {
+        public FileWriteStream(string path, FileShare share = FileShare.Read)
+            : base(path, FileMode.OpenOrCreate, FileAccess.Write, share)
+        { }
+    }
+}
