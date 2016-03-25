843afbe587c0643d24c7dd4d21e2b67ea04f5d25
hal1932 <yu.arai.19@gmail.com>
2016-03-26 05:23:55 +0900

    neue.ccさんの実装が便利なので取り込み

---
+++ HlLib/extensions/IEnumerableExtensions.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HlLib
+{
+    public static class IEnumerableExtensions
+    {
+        // http://neue.cc/2009/08/07_184.html
+        internal class CompareSelector<T, TKey> : IEqualityComparer<T>
+        {
+            private Func<T, TKey> selector;
+
+            public CompareSelector(Func<T, TKey> selector)
+            {
+                this.selector = selector;
+            }
+
+            public bool Equals(T x, T y)
+            {
+                return selector(x).Equals(selector(y));
+            }
+
+            public int GetHashCode(T obj)
+            {
+                return selector(obj).GetHashCode();
+            }
+        }
+
+        // http://neue.cc/2009/08/07_184.html
+        public static IEnumerable<T> Distinct<T, TKey>(this IEnumerable<T> source, Func<T, TKey> selector)
+        {
+            return source.Distinct(new CompareSelector<T, TKey>(selector));
+        }
+    }
+}
